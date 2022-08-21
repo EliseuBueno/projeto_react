0a2615414859518e709b29d1fc23c2478848c397
Eliseu Bueno <sturarobuenoeliseu@gmail.com>
2022-08-21 15:51:18 -0300

    Feat: Criação de primeiros componentes com props

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
   const name = 'Eliseu'
+  const idade = '33'
+  const profissao = 'Programador'
   const newName = name.toUpperCase()
   const url = 'https://via.placeholder.com/150'
   function sum(a, b){
@@ -14,11 +18,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Alterando o JSX</h1>
+        <SayMyName nome={name}/>
         <p>Olá, {newName}</p>
         <p>Soma: {2 + 2}</p>
         <p>Som por Função: {sum(1, 2)}</p>
         <img src={url} alt="Minha Imagem"/>
         <HelloWorld></HelloWorld>
+        <Pessoa nome={name} idade={idade} profissao={profissao} foto={url}/>
       </header>
     </div>
   );

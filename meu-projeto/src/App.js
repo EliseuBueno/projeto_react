11aaad926e907c67dcacf00537dafd12b45fdf09
Eliseu Bueno <sturarobuenoeliseu@gmail.com>
2022-08-21 15:27:47 -0300

    Feat: Criação de primeiros componentes

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
+  const name = 'Eliseu'
+  const newName = name.toUpperCase()
+  const url = 'https://via.placeholder.com/150'
+  function sum(a, b){
+    return a + b
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Alterando o JSX</h1>
+        <p>Olá, {newName}</p>
+        <p>Soma: {2 + 2}</p>
+        <p>Som por Função: {sum(1, 2)}</p>
+        <img src={url} alt="Minha Imagem"/>
+        <HelloWorld></HelloWorld>
       </header>
     </div>
   );
